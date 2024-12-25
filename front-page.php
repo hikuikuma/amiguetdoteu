22902cff7a36838cd7dc7a2a2fe444b4b3db57f4
Lionel Amiguet <lionel@amiguet.eu>
2024-12-25 17:05:04 +0100

    fix: Homepage

--- front-page.php
+++ front-page.php
@@ -1,18 +1,20 @@
 <?php get_header(); ?>
 
 <div class="hero-header">
-
+    <div class="hero-header__filter">
+        <h1 class="hero-header__titre">Créations numériques</h1>
+    </div>
+    <video src="<?= get_template_directory_uri().'/assets/video/heroheader_251224.webm'; ?>" class="hero-header__video" autoplay loop muted></video>
 </div>
 
 <main class="site-content site-width-content">
     <?php
-    	if (have_posts()) {
+        if (have_posts()) {
             while (have_posts()) {
                 the_post();
-	            the_content();
+                the_content();
             }
         }
     ?>
 </main>
-
 <?php get_footer(); ?>
